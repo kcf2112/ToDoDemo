85a0da028a992c0d8a1bcdf3f7b218535443c157
Kevin Filer <kcf2112@gmail.com>
2022-02-11 19:05:11 -0500

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Kevin Filer on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
